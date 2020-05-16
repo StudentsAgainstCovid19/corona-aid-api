6e49b5e3c28076ca3f541ebd148ad0055a9d0d93
Marc Auberer <marc.auberer@chillibits.com>
2020-05-16 20:00:04 +0200

    Fix test for GH Actions

--- src/test/kotlin/com/chillibits/coronaaid/CoronaAidApplicationTests.kt
+++ src/test/kotlin/com/chillibits/coronaaid/CoronaAidApplicationTests.kt
@@ -3,7 +3,6 @@ package com.chillibits.coronaaid
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class CoronaAidApplicationTests {
 
 	@Test

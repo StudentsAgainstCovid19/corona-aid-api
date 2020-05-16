e34f81a8a84a6a65c42155e3c3c1178d14532634
Marc Auberer <marc.auberer@chillibits.com>
2020-05-16 19:14:02 +0200

    Add CommandLineRunner test method

--- src/main/kotlin/com/chillibits/coronaaid/CoronaAidApplication.kt
+++ src/main/kotlin/com/chillibits/coronaaid/CoronaAidApplication.kt
@@ -1,10 +1,17 @@
 package com.chillibits.coronaaid
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CoronaAidApplication
+class CoronaAidApplication: CommandLineRunner {
+	override fun run(vararg args: String?) {
+		// Test db
+
+	}
+
+}
 
 fun main(args: Array<String>) {
 	runApplication<CoronaAidApplication>(*args)

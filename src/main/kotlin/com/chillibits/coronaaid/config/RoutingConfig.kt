17e548b2e4040d32ba3fc09620928c80b5630019
Marc Auberer <marc.auberer@chillibits.com>
2020-05-23 19:04:41 +0200

    Add controller for Swagger redirection

---
+++ src/main/kotlin/com/chillibits/coronaaid/config/RoutingConfig.kt
@@ -0,0 +1,16 @@
+package com.chillibits.coronaaid.config
+
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import springfox.documentation.annotations.ApiIgnore
+
+@Controller
+@ApiIgnore
+class RoutingConfig {
+    @RequestMapping(method = [RequestMethod.GET], value = ["/"])
+    @ApiOperation(value = "Redirects the user to the swagger ui page", hidden = true)
+    fun swagger() = "redirect:/swagger-ui.html"
+}

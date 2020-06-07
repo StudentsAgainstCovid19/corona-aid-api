a7ef3f7a1a34ce5460c9db742354f6abb48b9b02
Johannes Quast <johannesquast@outlook.de>
2020-06-07 02:19:54 +0200

    Add endpoint for symptoms (+tests)

---
+++ src/main/kotlin/com/chillibits/coronaaid/controller/v1/SymptomController.kt
@@ -0,0 +1,25 @@
+package com.chillibits.coronaaid.controller.v1
+
+import com.chillibits.coronaaid.model.dto.SymptomDto
+import com.chillibits.coronaaid.repository.SymptomRepository
+import com.chillibits.coronaaid.shared.toDto
+import io.swagger.annotations.Api
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@Api(value = "Symptoms Rest Endpoint", tags = ["symptoms"])
+class SymptomController {
+
+    @Autowired
+    private lateinit var symptomRepository: SymptomRepository
+
+    @GetMapping(
+            path = ["/symptom"],
+            produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE ]
+    )
+    fun getAllSymptoms(): List<SymptomDto> = symptomRepository.findAll().map { it.toDto() }
+
+}

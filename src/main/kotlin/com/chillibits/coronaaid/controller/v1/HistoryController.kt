ff4250e882344d645a28188aed3e326cd21589dd
Johannes Quast <johannesquast@outlook.de>
2020-06-08 02:19:57 +0200

    Add history controller + extensive unit tests

---
+++ src/main/kotlin/com/chillibits/coronaaid/controller/v1/HistoryController.kt
@@ -0,0 +1,77 @@
+package com.chillibits.coronaaid.controller.v1
+
+import com.chillibits.coronaaid.model.db.HistoryItem
+import com.chillibits.coronaaid.model.db.Symptom
+import com.chillibits.coronaaid.model.dto.HistoryItemDto
+import com.chillibits.coronaaid.model.dto.HistoryItemInsertDto
+import com.chillibits.coronaaid.repository.HistoryRepository
+import com.chillibits.coronaaid.repository.InfectedRepository
+import com.chillibits.coronaaid.repository.SymptomRepository
+import com.chillibits.coronaaid.shared.toDto
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@Api(value = "History REST Endpoint", tags = ["history"])
+class HistoryController {
+
+    @Autowired
+    private lateinit var historyRepository: HistoryRepository
+
+    @Autowired
+    private lateinit var symptomRepository: SymptomRepository
+
+    @Autowired
+    private lateinit var infectedRepository: InfectedRepository
+
+    @GetMapping(
+            path = ["/history"],
+            produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE ]
+    )
+    @ApiOperation("Returns all history items")
+    fun getAllHistoryItems(): List<HistoryItemDto> = historyRepository.findAll().map { it.toDto() }
+
+    @GetMapping(
+            path = ["/history/{infectedId}"],
+            produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE]
+    )
+    @ApiOperation("Returns all history items for a specific person")
+    fun getHistoryItemForSinglePerson(@PathVariable infectedId: Int): List<HistoryItemDto>
+            = historyRepository.getHistoryItemsForPerson(infectedId).map { it.toDto() } //TODO: Add infected not found check
+
+    @PostMapping(
+            path = ["/history"],
+            consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE],
+            produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE]
+    )
+    @ApiOperation("Pushes a new history item to the database")
+    @ApiResponses(
+            ApiResponse(code = 404, message = "Infected not found")
+    )
+    fun addHistoryItem(@RequestBody historyDto: HistoryItemInsertDto): ResponseEntity<HistoryItemDto> {
+        val infected = infectedRepository.findById(historyDto.infectedId)
+        if(infected.isEmpty) return ResponseEntity(HttpStatus.NOT_FOUND)
+
+        val symptoms = mutableListOf<Symptom>()
+        symptoms.addAll(symptomRepository.findAllById(historyDto.symptom)) //TODO: Null checks -> 404 Status code or throw exception
+
+        val item = HistoryItem(
+                0,
+                infected.get(),
+                historyDto.timestamp,
+                symptoms,
+                historyDto.status,
+                historyDto.personalFeeling
+        )
+
+        return ResponseEntity(historyRepository.save(item).toDto(), HttpStatus.CREATED)
+    }
+
+}
---
+++ src/test/kotlin/com/chillibits/coronaaid/controller/v1/HistoryControllerTests.kt
@@ -0,0 +1,119 @@
+package com.chillibits.coronaaid.controller.v1
+
+import com.chillibits.coronaaid.model.db.HistoryItem
+import com.chillibits.coronaaid.model.db.Infected
+import com.chillibits.coronaaid.model.db.Symptom
+import com.chillibits.coronaaid.model.dto.HistoryItemDto
+import com.chillibits.coronaaid.model.dto.HistoryItemInsertDto
+import com.chillibits.coronaaid.model.dto.SymptomDto
+import com.chillibits.coronaaid.repository.HistoryRepository
+import com.chillibits.coronaaid.repository.InfectedRepository
+import com.chillibits.coronaaid.repository.SymptomRepository
+import org.assertj.core.api.Assertions
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+import java.time.LocalDate
+import java.time.Month
+import java.util.*
+
+@RunWith(SpringRunner::class)
+@ActiveProfiles("logging")
+@DisplayName("Config Controller")
+class HistoryControllerTests {
+
+    @Autowired
+    private lateinit var historyController: HistoryController
+
+    @MockBean
+    private lateinit var historyRepository: HistoryRepository
+
+    @MockBean
+    private lateinit var symptomRepository: SymptomRepository
+
+    @MockBean
+    private lateinit var infectedRepository: InfectedRepository
+
+    private val dummyInfected = getDummyInfected()
+    private val testData = getHistoryTestData()
+    private val assertData = getHistoryAssertData()
+
+    @TestConfiguration
+    class HistoryControllerImplTestContextConfiguration {
+
+        @Bean
+        fun historyController() = HistoryController()
+
+    }
+
+    @Before
+    fun init() {
+        //history repository
+        Mockito.`when`(historyRepository.findAll()).thenReturn(testData)
+        Mockito.`when`(historyRepository.getHistoryItemsForPerson(5)).thenReturn(listOf(testData[0]))
+        Mockito.`when`(historyRepository.save(getTestInsertRepositoryDao())).thenReturn(getTestInsertRepositoryResultDao())
+
+        //symptom repository
+        Mockito.`when`(symptomRepository.findById(0)).thenReturn(Optional.of(getSymptomTestData()[0]))
+        Mockito.`when`(symptomRepository.findAllById(listOf(0))).thenReturn(getSymptomTestData())
+
+        //infected repository
+        Mockito.`when`(infectedRepository.findById(5)).thenReturn(Optional.of(getDummyInfected()))
+    }
+
+    @Test
+    fun testGetAllHistoryItems() {
+        Assertions.assertThat(historyController.getAllHistoryItems()).containsExactlyInAnyOrder(assertData[0], assertData[1])
+    }
+
+    @Test
+    fun testGetHistoryItemsOfSinglePerson() {
+        Assertions.assertThat(historyController.getHistoryItemForSinglePerson(5)).isEqualTo(listOf(assertData[0]))
+    }
+
+    @Test
+    fun testPostSingleHistoryItem() {
+        val result = historyController.addHistoryItem(getTestInsertInputDto())
+        Assertions.assertThat(result).isEqualTo(ResponseEntity(getTestInsertOutputDto(), HttpStatus.CREATED))
+    }
+
+    @Test
+    fun testPostSingleHistoryItemWithUnknownInfected() {
+        val result = historyController.addHistoryItem(getTestInsertUnknownInfectedDto())
+        Assertions.assertThat(result).isEqualTo(ResponseEntity<HistoryItemDto>(HttpStatus.NOT_FOUND))
+    }
+
+    fun getDummyInfected() = Infected(5, "Donald", "Trump", LocalDate.of(1900, Month.JANUARY, 10), "Pjöngjang", "kim-111", "Nuclearstreet", "666", 420.0, 360.0, emptyList(), emptyList(), emptyList(), emptyList())
+
+    fun getHistoryTestData(): List<HistoryItem> {
+        val historyItem1 = HistoryItem(0, getDummyInfected(), 1234, emptyList(), 0, 1)
+        val historyItem2 = HistoryItem(1, getDummyInfected(), 4321, emptyList(), 1, 0)
+        return listOf(historyItem1, historyItem2)
+    }
+
+    fun getHistoryAssertData() : List<HistoryItemDto> {
+        val historyItem1 = HistoryItemDto(0, getDummyInfected().id, 1234, emptyList(), 0, 1)
+        val historyItem2 = HistoryItemDto(1, getDummyInfected().id, 4321, emptyList(), 1, 0)
+        return listOf(historyItem1, historyItem2)
+    }
+
+    fun getSymptomTestData(): List<Symptom> = listOf(Symptom(0, emptyList(), "fever", 7, 80))
+    fun getSymptomAssertData(): List<SymptomDto> = listOf(SymptomDto(0, "fever", 7, 80))
+
+    fun getTestInsertInputDto(): HistoryItemInsertDto = HistoryItemInsertDto(getDummyInfected().id, 9999, listOf(0), 2, 5)
+    fun getTestInsertRepositoryDao(): HistoryItem = HistoryItem(0, getDummyInfected(), 9999, listOf(getSymptomTestData()[0]), 2, 5)
+    fun getTestInsertRepositoryResultDao(): HistoryItem = HistoryItem(100, getDummyInfected(), 9999, listOf(getSymptomTestData()[0]), 2, 5)
+    fun getTestInsertOutputDto() : HistoryItemDto = HistoryItemDto(100, getDummyInfected().id, 9999, listOf(getSymptomAssertData()[0]), 2, 5)
+
+    fun getTestInsertUnknownInfectedDto(): HistoryItemInsertDto = HistoryItemInsertDto(919191, 9999, listOf(0), 2, 5)
+}

b674850adb09e0cd8c3b3f6677ad2b3b7a964af7
Johannes Quast <johannesquast@outlook.de>
2020-06-08 02:21:24 +0200

    Add query function for the history items of one specific person

--- src/main/kotlin/com/chillibits/coronaaid/repository/HistoryRepository.kt
+++ src/main/kotlin/com/chillibits/coronaaid/repository/HistoryRepository.kt
@@ -1,6 +1,13 @@
 package com.chillibits.coronaaid.repository
 
 import com.chillibits.coronaaid.model.db.HistoryItem
+import com.chillibits.coronaaid.model.dto.HistoryItemDto
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
-interface HistoryRepository: JpaRepository<HistoryItem, Int>
+interface HistoryRepository: JpaRepository<HistoryItem, Int> {
+
+    @Query("SELECT it FROM HistoryItem it, Infected i WHERE it.infectedId = i.id AND i.id = ?1")
+    fun getHistoryItemsForPerson(infectedId : Int): List<HistoryItem>
+
+}

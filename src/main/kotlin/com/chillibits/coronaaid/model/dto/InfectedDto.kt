d12f142c5dc3e3ad8a1065de1e840b9fa0032434
Johannes Quast <johannesquast@outlook.de>
2020-06-10 00:49:46 +0200

    Add 'notes' and 'health_insurance_number' column for Infected data model

--- src/main/kotlin/com/chillibits/coronaaid/model/dto/InfectedDto.kt
+++ src/main/kotlin/com/chillibits/coronaaid/model/dto/InfectedDto.kt
@@ -14,6 +14,8 @@ data class InfectedDto (
         val houseNumber: String,
         val lat: Double,
         val lon: Double,
+        val healthInsuranceNumber: String,
+        val notes : String?,
         val contactData: List<ContactItemDto>,
         val tests: List<TestDto>,
         val initialDiseases: List<InitialDiseaseDto>,

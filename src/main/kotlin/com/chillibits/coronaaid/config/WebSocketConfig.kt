1225794a57bda7933bf8f199ef9802b1241d1fa9
Marc Auberer <marc.auberer@chillibits.com>
2020-06-23 13:13:35 +0200

    Add new site to CORS policy

--- src/main/kotlin/com/chillibits/coronaaid/config/WebSocketConfig.kt
+++ src/main/kotlin/com/chillibits/coronaaid/config/WebSocketConfig.kt
@@ -13,10 +13,9 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 class WebSocketConfig : WebSocketConfigurer {
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry.addHandler(infectedHandler(), "/realtime/infected").setAllowedOrigins("http://localhost:63342", "http://localhost:63343", "https://sac19.jatsqi.com", "https://dev.sac19.jatsqi.com")
+        registry.addHandler(infectedHandler(), "/realtime/infected").setAllowedOrigins("http://localhost:63342", "http://localhost:63343", "https://sac19.jatsqi.com", "https://dev.sac19.jatsqi.com", "https://corona-aid-ka.de", "https://dev.corona-aid-ka.de")
     }
 
     @Bean
     fun infectedHandler(): WebSocketHandler = SocketTextHandler()
-
 }

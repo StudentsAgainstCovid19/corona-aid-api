6c3ae34d210fb0777c0a1618d954f1bb22a477db
Johannes Quast <johannesquast@outlook.de>
2020-06-24 12:03:10 +0200

    Disable websocket endpoint for the moment

--- src/main/kotlin/com/chillibits/coronaaid/config/WebSocketConfig.kt
+++ src/main/kotlin/com/chillibits/coronaaid/config/WebSocketConfig.kt
@@ -10,7 +10,6 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
-@EnableWebSocket
 class WebSocketConfig : WebSocketConfigurer {
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {

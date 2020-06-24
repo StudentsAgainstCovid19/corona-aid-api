a4a272fb96c87f6c46041e694c41e1bbd46ab6bf
Johannes Quast <johannesquast@outlook.de>
2020-06-24 12:02:43 +0200

    Add additional query methods

--- src/main/kotlin/com/chillibits/coronaaid/repository/HistoryRepository.kt
+++ src/main/kotlin/com/chillibits/coronaaid/repository/HistoryRepository.kt
@@ -1,6 +1,7 @@
 package com.chillibits.coronaaid.repository
 
 import com.chillibits.coronaaid.model.db.HistoryItem
+import org.springframework.cache.annotation.CachePut
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
@@ -9,4 +10,6 @@ interface HistoryRepository: JpaRepository<HistoryItem, Int> {
     @Query("SELECT it FROM HistoryItem it WHERE it.infectedId.id = ?1")
     fun getHistoryItemsForPerson(infectedId: Int): Set<HistoryItem>
 
+    @Query("SELECT it.id FROM HistoryItem it WHERE it.timestamp >= ?1")
+    fun getHistoryItemIdChangedSince(timestamp: Long): Set<Int>
 }

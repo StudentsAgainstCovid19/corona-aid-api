60890ada100d08078de9fee03e681ff947d249d6
Johannes Quast <johannesquast@outlook.de>
2020-06-24 12:02:26 +0200

    Add cron job for SSE

--- src/main/kotlin/com/chillibits/coronaaid/config/CronJobsConfig.kt
+++ src/main/kotlin/com/chillibits/coronaaid/config/CronJobsConfig.kt
@@ -1,11 +1,18 @@
 package com.chillibits.coronaaid.config
 
+import com.chillibits.coronaaid.model.dto.InfectedRealtimeDto
+import com.chillibits.coronaaid.repository.HistoryRepository
 import com.chillibits.coronaaid.repository.InfectedRepository
+import com.chillibits.coronaaid.shared.SseEmitterStorage
+import com.chillibits.coronaaid.shared.toCompressed
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.transaction.annotation.Transactional
+import java.io.StringWriter
 import javax.annotation.PostConstruct
 
 @Configuration
@@ -13,6 +20,9 @@ class CronJobsConfig {
 
     @Autowired
     private lateinit var infectedRepository: InfectedRepository
+
+    @Autowired
+    private lateinit var historyRepository: HistoryRepository
 
     val log: Logger = LoggerFactory.getLogger(CronJobsConfig::class.java)
 
@@ -30,5 +40,28 @@ class CronJobsConfig {
         log.info("Daily reset started.")
         infectedRepository.resetLockingOfAllInfected()
         log.info("Daily reset finished.")
+    }
+
+    @Scheduled(fixedDelay = 15000)
+    @Transactional
+    public fun sendInterval() {
+        val list = mutableSetOf<InfectedRealtimeDto>()
+        val changed = historyRepository
+                .getHistoryItemIdChangedSince(System.currentTimeMillis() - 15000)
+                .plus(infectedRepository.findAllLockedSince(System.currentTimeMillis() - 15000))
+        val infected = infectedRepository.findAllEagerly(changed)
+
+        val writer = StringWriter()
+        val mapper = ObjectMapper()
+        mapper.writeValue(writer, infected.map { it.toCompressed() }.map {
+            InfectedRealtimeDto(
+                    it.id,
+                    it.done,
+                    it.lastUnsuccessfulCallToday != null,
+                    it.lastUnsuccessfulCallTodayString,
+                    false
+            )
+        })
+        SseEmitterStorage.sendToAll(writer.toString())
     }
 }

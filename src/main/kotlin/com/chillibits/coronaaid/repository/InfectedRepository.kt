3767e07863fb9a7ac1381c7e26c80ab2f84aca7d
Johannes Quast <johannesquast@outlook.de>
2020-06-26 11:02:49 +0200

    Fix time frame bug

--- src/main/kotlin/com/chillibits/coronaaid/repository/InfectedRepository.kt
+++ src/main/kotlin/com/chillibits/coronaaid/repository/InfectedRepository.kt
@@ -24,17 +24,12 @@ interface InfectedRepository: JpaRepository<Infected, Int> {
     @Query("SELECT i FROM Infected i WHERE i.id IN (:infected)")
     fun findAllEagerly(@Param("infected") infected: Set<Int>): Set<Infected>
 
-    @Query("SELECT i.id FROM Infected i WHERE ((i.lockedTimestamp >= ?1) OR (i.lockedTimestamp + ?2 >= ?1))")
-    fun findAllLockedSince(timestamp: Long, configAutoResetOffset: Long): Set<Int>
+    @Query("SELECT i.id FROM Infected i WHERE ((i.lockedTimestamp >= ?1) OR ((i.lockedTimestamp + ?2 >= ?1) AND (i.lockedTimestamp + ?2 <= ?1 + ?3)))")
+    fun findAllLockedSince(timestamp: Long, configAutoResetOffset: Long, refreshInterval: Long): Set<Int>
 
     @Modifying
     @Transactional
     @Query("UPDATE Infected i SET i.lockedTimestamp = ?2 WHERE i.id = ?1")
     fun changeLockedState(infectedId: Int, timestamp: Long)
-
-    @Modifying
-    @Transactional
-    @Query("UPDATE Infected i SET i.lockedTimestamp = 0")
-    fun resetLockingOfAllInfected()
 
 }

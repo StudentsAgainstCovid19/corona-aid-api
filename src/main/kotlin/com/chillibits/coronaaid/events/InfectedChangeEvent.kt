b2f7070c98c9231db83878343cfb1eacce380b63
Johannes Quast <johannesquast@outlook.de>
2020-06-26 11:02:40 +0200

    Improve code style

--- src/main/kotlin/com/chillibits/coronaaid/events/InfectedChangeEvent.kt
+++ src/main/kotlin/com/chillibits/coronaaid/events/InfectedChangeEvent.kt
@@ -1,6 +1,5 @@
 package com.chillibits.coronaaid.events
 
-import com.chillibits.coronaaid.model.db.Infected
 import org.springframework.context.ApplicationEvent
 
-class InfectedChangeEvent(source: Any, val changedInfected : Set<Int>) : ApplicationEvent(source)
+class InfectedChangeEvent(source: Any, val changedInfected: Set<Int>) : ApplicationEvent(source)

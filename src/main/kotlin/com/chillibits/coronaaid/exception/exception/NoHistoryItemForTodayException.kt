4212d5f2ad057338c263937667b3e6771c6b1cb0
Marc Auberer <marc.auberer@sap.com>
2020-06-27 20:28:58 +0200

    Fix grammar mistake

--- src/main/kotlin/com/chillibits/coronaaid/exception/exception/NoHistoryItemForTodayException.kt
+++ src/main/kotlin/com/chillibits/coronaaid/exception/exception/NoHistoryItemForTodayException.kt
@@ -1,4 +1,4 @@
 package com.chillibits.coronaaid.exception.exception
 
 class NoHistoryItemForTodayException:
-        RuntimeException("No HistoryItem for today found. Nothing to update.")
+        RuntimeException("No HistoryItem found for today. Nothing to update.")

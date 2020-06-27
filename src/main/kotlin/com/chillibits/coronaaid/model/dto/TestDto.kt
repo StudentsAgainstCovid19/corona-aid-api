55b84ae04b8caaf93ac97cbcbf98358943390458
Johannes Quast <johannesquast@outlook.de>
2020-06-27 12:17:49 +0200

    Add day difference to TestDto

--- src/main/kotlin/com/chillibits/coronaaid/model/dto/TestDto.kt
+++ src/main/kotlin/com/chillibits/coronaaid/model/dto/TestDto.kt
@@ -1,8 +1,13 @@
 package com.chillibits.coronaaid.model.dto
 
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
 data class TestDto (
         val id: Int,
         val infectedId : Int?,
         val timestamp: Long,
         val result: Int
-)
+) {
+    val daysOverdue: Long = ChronoUnit.DAYS.between(Instant.ofEpochMilli(timestamp), Instant.now())
+}

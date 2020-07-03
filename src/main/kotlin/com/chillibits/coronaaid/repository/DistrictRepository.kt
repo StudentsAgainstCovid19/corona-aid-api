6bfe76f5ad22b152204a6701a77d19ea0508bf84
Johannes Quast <johannesquast@outlook.de>
2020-07-03 16:04:12 +0200

    Add repository for districts

---
+++ src/main/kotlin/com/chillibits/coronaaid/repository/DistrictRepository.kt
@@ -0,0 +1,18 @@
+package com.chillibits.coronaaid.repository
+
+import com.chillibits.coronaaid.model.db.District
+import com.chillibits.coronaaid.model.dto.DistrictAnalyticsDto
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+
+interface DistrictRepository: JpaRepository<District, Int> {
+
+    @Query(
+            "SELECT " +
+            "NEW com.chillibits.coronaaid.model.dto.DistrictAnalyticsDto(dis.name, dis.city, 0.0+ST_Area(dis.geometry), dis.postalCode, COUNT(inf), dis.geometry) " +
+            "FROM District dis LEFT JOIN Infected inf ON dis.city=inf.city " +
+            "WHERE inf.postalCode=dis.postalCode AND true=ST_Contains(dis.geometry, ST_SRID(POINT(inf.lon, inf.lat), 4326)) " +
+            "GROUP BY dis.id")
+    fun getAnalyzedDistrictData() : List<DistrictAnalyticsDto>
+
+}

ea4c7e5a676b0635b87f8b2043cab0d3c4d34352
Johannes Quast <johannesquast@outlook.de>
2020-07-03 16:23:29 +0200

    Change return type

--- src/main/kotlin/com/chillibits/coronaaid/repository/DistrictRepository.kt
+++ src/main/kotlin/com/chillibits/coronaaid/repository/DistrictRepository.kt
@@ -9,10 +9,10 @@ interface DistrictRepository: JpaRepository<District, Int> {
 
     @Query(
             "SELECT " +
-            "NEW com.chillibits.coronaaid.model.dto.DistrictAnalyticsDto(dis.name, dis.city, 0.0+ST_Area(dis.geometry), dis.postalCode, COUNT(inf), dis.geometry) " +
+            "NEW com.chillibits.coronaaid.model.dto.DistrictAnalyticsDto(dis.id, dis.name, dis.city, 0.0+ST_Area(dis.geometry), dis.postalCode, COUNT(inf), dis.geometry) " +
             "FROM District dis LEFT JOIN Infected inf ON dis.city=inf.city " +
             "WHERE inf.postalCode=dis.postalCode AND true=ST_Contains(dis.geometry, ST_SRID(POINT(inf.lon, inf.lat), 4326)) " +
             "GROUP BY dis.id")
-    fun getAnalyzedDistrictData() : List<DistrictAnalyticsDto>
+    fun getAnalyzedDistrictData() : Set<DistrictAnalyticsDto>
 
 }
